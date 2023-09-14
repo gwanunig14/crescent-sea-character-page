6d7302ea44e713591a60805d7e1012104b28d3e4
TJ Mercer <ramblingwanunig@gmail.com>
2023-09-14 00:13:34 -0400

    Working

--- src/CharacterManagement/CharacterSheetSections/SkillsSection.jsx
+++ src/CharacterManagement/CharacterSheetSections/SkillsSection.jsx
@@ -43,29 +43,26 @@ function SkillDetail({ skill, success, modifier }) {
   }
 }
 
-function SkillsSection(heading, modifier, skills) {
-  const strings = SkillStrings;
-  debugger;
-  return null;
-  //   return (
-  //     <div>
-  //       <div>
-  //         <div>{SkillStrings[heading].name}</div>
-  //         <div>{"success " + modifier}</div>
-  //         <div>{"special success " + Math.ceil(modifier / 5)}</div>
-  //         <div>{"critical " + Math.ceil(modifier / 20)}</div>
-  //         <div>{"failure " + (100 - Math.ceil((100 - modifier) / 20))}</div>
-  //       </div>
-  //       {Object.keys(skills).map((skill) => (
-  //         <SkillDetail
-  //           key={skill}
-  //           skill={skill}
-  //           success={skills[skill]}
-  //           modifier={modifier}
-  //         />
-  //       ))}
-  //     </div>
-  //   );
+function SkillsSection({ heading, modifier, skills }) {
+  return (
+    <div>
+      <div>
+        <div>{SkillStrings[heading].name}</div>
+        <div>{"success " + modifier}</div>
+        <div>{"special success " + Math.ceil(modifier / 5)}</div>
+        <div>{"critical " + Math.ceil(modifier / 20)}</div>
+        <div>{"failure " + (100 - Math.ceil((100 - modifier) / 20))}</div>
+      </div>
+      {Object.keys(skills).map((skill) => (
+        <SkillDetail
+          key={skill}
+          skill={skill}
+          success={skills[skill]}
+          modifier={modifier}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default SkillsSection;
