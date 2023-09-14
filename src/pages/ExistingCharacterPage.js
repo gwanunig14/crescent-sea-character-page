426f1aadc94ad6947d5ac2271d8345d571356955
TJ Mercer <ramblingwanunig@gmail.com>
2023-09-14 11:47:24 -0400

    Existing character page

---
+++ src/pages/ExistingCharacterPage.js
@@ -0,0 +1,13 @@
+// src/components/ExistingCharacterPage.js
+import React from "react";
+
+function ExistingCharacterPage() {
+  return (
+    <div>
+      <h1>Existing Character Page</h1>
+      {/* Add code to display existing characters */}
+    </div>
+  );
+}
+
+export default ExistingCharacterPage;
