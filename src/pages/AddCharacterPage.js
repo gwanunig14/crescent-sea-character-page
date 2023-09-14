af9bc54b84344bb8412d97e6d64430eb40626d0d
TJ Mercer <ramblingwanunig@gmail.com>
2023-09-14 11:48:29 -0400

    Add character page

---
+++ src/pages/AddCharacterPage.js
@@ -0,0 +1,13 @@
+// src/components/AddCharacterPage.js
+import React from "react";
+
+function AddCharacterPage() {
+  return (
+    <div>
+      <h1>Add Character Page</h1>
+      {/* Add code to add a new character */}
+    </div>
+  );
+}
+
+export default AddCharacterPage;
